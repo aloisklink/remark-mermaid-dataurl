aloisklink/remark-mermaid-dataurl

commit fc2f31e49403c0bf6f09501b3ce5f408e2f441f3

fix: support mermaid v9.1.6
Simplifies the code by calling the new mermaid-cli Node.JS API
directly, which was introduced in Mermaid v9.1.6.

In the future, it should be possible to optimise the plugin
by reusing the same browser instance too.

index.js
@@ -1,19 +1,11 @@
-const { Volume } = require("memfs");
-const childProcess = require("child_process");
-const path = require("path");
+const { readFile } = require("fs/promises");
+const puppeteer = require("puppeteer");
 
 const visit = require("unist-util-visit");
-const mmdc = require.resolve("@mermaid-js/mermaid-cli/index.bundle.js");
 
 const { setSvgBbox, validSVG } = require("./src/svg.js");
 
 const PLUGIN_NAME = "remark-mermaid-dataurl";
-
-const createVolume = () => {
-  const volume = new Volume();
-  volume.mkdirSync(process.cwd(), { recursive: true });
-  return volume;
-};
 
 /**
  * Calls mmdc (mermaid-cli) with the given keyword args.
@@ -29,104 +21,35 @@ const createVolume = () => {
  * @throws {Error} If mmdc fails in anyways.
  * @returns {Promise<string>} Returns the rendered mermaid code as an SVG.
  */
-function renderMermaidFile(kwargs, input) {
-  const volume = createVolume();
-  const cwd = process.cwd();
-  const outputPath = path.join(cwd, "output.svg");
-  const inputPath = path.join(cwd, "input");
-  volume.writeFileSync(inputPath, input, "utf8");
-
-  if (kwargs.configFile && typeof kwargs.configFile === "object") {
-    const configFilePath = path.join(cwd, "config.json");
-    volume.writeFileSync(
-      configFilePath,
-      JSON.stringify(kwargs.configFile),
-      "utf8"
+async function renderMermaidFile(kwargs, input) {
+  let configFile = kwargs.configFile ?? {};
+  if (kwargs.configFile && typeof kwargs.configFile !== "object") {
+    configFile = JSON.parse(
+      await readFile(kwargs.configFile, { encoding: "utf8" })
     );
-    kwargs = { ...kwargs, configFile: configFilePath };
   }
+  let puppeteerConfigFile = kwargs.puppeteerConfigFile ?? {};
   if (
     kwargs.puppeteerConfigFile &&
-    typeof kwargs.puppeteerConfigFile === "object"
+    typeof kwargs.puppeteerConfigFile !== "object"
   ) {
-    const puppeteerConfigFilePath = path.join(cwd, "puppeteerConfigFile.json");
-    volume.writeFileSync(
-      puppeteerConfigFilePath,
-      JSON.stringify(kwargs.puppeteerConfigFile),
-      "utf8"
+    puppeteerConfigFile = JSON.parse(
+      await readFile(kwargs.puppeteerConfigFile, { encoding: "utf8" })
     );
-    kwargs = { ...kwargs, puppeteerConfigFile: puppeteerConfigFilePath };
   }
 
-  // Array.flatMap() was only added in NodeJS v11
-  const args = [].concat(
-    ...Object.entries({
-      ...kwargs,
-      input: inputPath,
-      output: outputPath,
-    }).map(([key, value]) => [`--${key}`, value])
-  );
-  const child_process = childProcess.fork(
-    require.resolve("./mermaid_hook"),
-    args,
-    {
-      // store stderr for errors, and ipc for piping memfs info
-      stdio: ["ignore", "ignore", "pipe", "ipc"],
-    }
-  );
-
-  const stderrChunks = [];
-  child_process.send(volume.toJSON());
-
-  child_process.stderr.on("data", (chunk) => stderrChunks.push(chunk));
-
-  return new Promise((resolve, reject) => {
-    let exited = false; // stream may error AND exit
-    child_process.on("message", (message) => {
-      exited = true;
-      child_process.kill();
-      volume.fromJSON(message);
-      resolve(volume.promises.readFile(outputPath, { encoding: "utf8" }));
+  // eslint-disable-next-line node/no-unsupported-features/es-syntax, node/no-missing-import
+  const { parseMMD } = await import("@mermaid-js/mermaid-cli");
+  const browser = await puppeteer.launch(puppeteerConfigFile);
+  try {
+    const outputSvg = await parseMMD(browser, input, "svg", {
+      mermaidConfig: configFile,
+      viewport: { width: 800, height: 600 },
     });
-    child_process.on("error", (error) => {
-      exited = true;
-      reject(error);
-    });
-    child_process.on("exit", (code, signal) => {
-      if (exited) {
-        return; // already resolved Promise
-      }
-      if (code) {
-        reject(
-          new Error(
-            `${mmdc} with kwargs ${JSON.stringify(
-              kwargs
-            )} failed with error code: ${code} and stderr: ${Buffer.concat(
-              stderrChunks
-            ).toString("utf-8")}`
-          )
-        );
-      } else if (signal) {
-        reject(
-          new Error(
-            `${mmdc} with kwargs ${JSON.stringify(
-              kwargs
-            )} recieved signal ${signal}`
-          )
-        );
-      } else {
-        // Mermaid-CLI throws unhandledRejection warnings
-        // which sometimes return exit code 0
-        reject(
-          new Error(
-            `${mmdc} with kwargs ${JSON.stringify(
-              kwargs
-            )} exited without returning created SVG.`
-          )
-        );
-      }
-    });
-  });
+    return outputSvg.toString("utf8");
+  } finally {
+    await browser.close();
+  }
 }
 
 /** Converts a string to a base64 string */

.eslintrc.js
@@ -1,3 +1,6 @@
 module.exports = {
   extends: ["plugin:prettier/recommended", "plugin:node/recommended"],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
 };

jest.config.js
@@ -11,4 +11,8 @@ module.exports = {
   ],
   // work around for https://github.com/nodejs/node/issues/35889#issuecomment-1129293091
   runner: "jest-light-runner",
+  // from https://github.com/chalk/chalk/issues/532
+  "moduleNameMapper": {
+    "#(.*)": "<rootDir>/node_modules/$1"
+  },
 };

mermaid_hook.js
@@ -1,61 +1,0 @@
-const { Volume } = require("memfs");
-const fs = require("fs");
-const { callbackify } = require("util");
-
-const mermaid_cli = require.resolve("@mermaid-js/mermaid-cli/index.bundle.js");
-
-const patch_fs = (fs, volume, methods = []) => {
-  const bkup = {};
-  methods.forEach((method) => {
-    bkup[method] = fs[method];
-    fs[method] = volume[method].bind(volume);
-  });
-  return () => {
-    Object.keys(bkup).forEach((method) => {
-      fs[method] = bkup[method];
-    });
-  };
-};
-
-process.once("message", function (volumeJSON) {
-  const virtualVolume = Volume.fromJSON(volumeJSON);
-  virtualVolume.mkdirSync(process.cwd(), { recursive: true });
-
-  // bind these functions to the original unpatched fs
-  const existsSync = fs.existsSync.bind(fs);
-  const readFile = fs.readFile.bind(fs);
-  const readFileSync = fs.readFileSync.bind(fs);
-
-  patch_fs(
-    fs,
-    {
-      writeFileSync: (...args) => {
-        virtualVolume.writeFileSync(...args);
-        process.send(virtualVolume.toJSON());
-      },
-      writeFile: callbackify((...args) => {
-        virtualVolume.writeFileSync(...args);
-        process.send(virtualVolume.toJSON());
-        return Promise.resolve();
-      }),
-      existsSync: (path) => {
-        return existsSync(path) || virtualVolume.existsSync(path);
-      },
-      readFile: (path, opts, cb) => {
-        if (virtualVolume.existsSync(path)) {
-          return virtualVolume.readFile(path, opts, cb);
-        }
-        readFile(path, opts, cb);
-      },
-      readFileSync: (path, opts) => {
-        if (virtualVolume.existsSync(path)) {
-          return virtualVolume.readFileSync(path, opts);
-        }
-        return readFileSync(path, opts);
-      },
-    },
-    ["writeFileSync", "existsSync", "readFile", "writeFile", "readFileSync"]
-  );
-
-  require(mermaid_cli);
-});

commit 6105b425b43519f8a33cdcdf2779eefaecada2eb

fix: fix some mermaidCli options being ignored
Some mermaidCli options (like backgroundColor/width/height)
were not being sent over the mermaidCli's new
parseMMD function.

Fixes: fc2f31e49403c0bf6f09501b3ce5f408e2f441f3

index.js
@@ -8,6 +8,46 @@ const { setSvgBbox, validSVG } = require("./src/svg.js");
 const PLUGIN_NAME = "remark-mermaid-dataurl";
 
 /**
+ * Converts CLI args to {@link parseMMD} options.
+ *
+ * Required for backwards compatibility.
+ *
+ * @param {{[key: string]: any}} kwargs Args passed to mmdc in format `--key value`
+ * @returns {Promise<import("@mermaid-js/mermaid-cli").ParseMDDOptions>} Options to pass to parseMMD.
+ */
+async function convertMermaidKwargsToParseMMDOpts({
+  theme,
+  width = 800,
+  height = 600,
+  backgroundColor,
+  configFile,
+  cssFile,
+  scale,
+  pdfFit,
+}) {
+  let mermaidConfig = { theme };
+  if (configFile && typeof configFile !== "object") {
+    mermaidConfig = {
+      ...mermaidConfig,
+      ...JSON.parse(await readFile(configFile, { encoding: "utf8" })),
+    };
+  }
+
+  let myCSS;
+  if (cssFile) {
+    myCSS = await readFile(cssFile, { encoding: "utf8" });
+  }
+
+  return {
+    mermaidConfig,
+    backgroundColor,
+    myCSS,
+    pdfFit,
+    viewport: { width, height, deviceScaleFactor: scale },
+  };
+}
+
+/**
  * Calls mmdc (mermaid-cli) with the given keyword args.
  *
  * This function runs `mmdc` in a separate process.
@@ -22,12 +62,6 @@ const PLUGIN_NAME = "remark-mermaid-dataurl";
  * @returns {Promise<string>} Returns the rendered mermaid code as an SVG.
  */
 async function renderMermaidFile(kwargs, input) {
-  let configFile = kwargs.configFile ?? {};
-  if (kwargs.configFile && typeof kwargs.configFile !== "object") {
-    configFile = JSON.parse(
-      await readFile(kwargs.configFile, { encoding: "utf8" })
-    );
-  }
   let puppeteerConfigFile = kwargs.puppeteerConfigFile ?? {};
   if (
     kwargs.puppeteerConfigFile &&
@@ -42,10 +76,12 @@ async function renderMermaidFile(kwargs, input) {
   const { parseMMD } = await import("@mermaid-js/mermaid-cli");
   const browser = await puppeteer.launch(puppeteerConfigFile);
   try {
-    const outputSvg = await parseMMD(browser, input, "svg", {
-      mermaidConfig: configFile,
-      viewport: { width: 800, height: 600 },
-    });
+    const outputSvg = await parseMMD(
+      browser,
+      input,
+      "svg",
+      await convertMermaidKwargsToParseMMDOpts(kwargs)
+    );
     return outputSvg.toString("utf8");
   } finally {
     await browser.close();

test/index.test.mjs
@@ -190,4 +190,18 @@ describe("test markdown files", () => {
     },
     timeout
   );
+
+  test(
+    "should set a red background",
+    async () => {
+      await testScreenshotSnapshot("test/fixtures/basic.in.md", {
+        remarkOptions: {
+          mermaidCli: {
+            backgroundColor: "red",
+          },
+        },
+      });
+    },
+    timeout
+  );
 });