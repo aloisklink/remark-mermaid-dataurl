2524a8ddf83dd4fd6313c0ae6ce88bc10c930cbc
Alois Klink <alois@aloisklink.com>
2022-11-19 23:16:12 +0000

    test: handle mermaid v9.2.2 new error types
    Mermaid now throws 'No diagram type detected for text'
    for invalid mermaid diagrams.

--- test/index.test.mjs
+++ test/index.test.mjs
@@ -158,7 +158,7 @@ describe("test markdown files", () => {
     async () => {
       await expect(
         testScreenshotSnapshot("test/fixtures/invalid.in.md")
-      ).rejects.toThrow("Parse error");
+      ).rejects.toThrow("No diagram type detected for text");
     },
     timeout
   );
